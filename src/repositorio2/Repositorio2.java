fa5d150d85e292e64dd618706db02a927b185f77
Erick Párraga <Erick Prraga@Laptop-de-Erick>
2025-08-19 13:47:00 -0500

    Probando mi primer repositorio en Github

---
+++ src/repositorio2/Repositorio2.java
@@ -0,0 +1,19 @@
+
+package repositorio2;
+
+/**
+ *
+ * @author Erick Párraga
+ */
+public class Repositorio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola, soy Erick y este es mi 1er repositorio");
+        // TODO code application logic here
+    }
+    
+}
